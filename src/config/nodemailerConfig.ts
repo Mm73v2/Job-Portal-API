aceb49451aac29795bf1a1cf4680d6182c7da265
Mm73v2 <mohamedabobakr04@gmail.com>
2025-02-28 17:56:11 +0200

    Remove Google OAuth credentials

--- src/config/nodemailerConfig.ts
+++ src/config/nodemailerConfig.ts
@@ -1,17 +1,17 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+const NODE_MAILER_AUTH_USER = process.env.NODE_MAILER_AUTH_USER;
+const NODE_MAILER_AUTH_PASS = process.env.NODE_MAILER_AUTH_USER;
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true for port 465, false for other ports
+  secure: false,
   auth: {
-    // type: "OAuth2",
-    user: "mohamedabobakr04@gmail.com",
-    pass: "puhewvdkofggjmxt",
-    // clientId:
-    //   "172995117574-60n90ki7cd974aur5br22akfg8q8b2qg.apps.googleusercontent.com",
-    // clientSecret: "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",
-    // refreshToken:
-    //   "1//0418NdMSG1F2BCgYIARAAGAQSNwF-L9Ir40qhyLqy4lh3oHaErnj_htndsT5SZnqms00yfHG3tyuTFcTD2xUSZe1drtywA03fg0E",
+    user: NODE_MAILER_AUTH_USER,
+    pass: NODE_MAILER_AUTH_PASS,
   },
 });
 
