d196762e5bdce689ac9ea22d62c90b7afb3faa2a
Mmv73v2 <mohamedaobakr04@gmail.com>
2025-03-08 22:23:21 +0200

    Add applications controller with getAllApplications and createApplication methods

---
+++ src/controllers/applicationsController.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from "express";
+import asyncWrapper from "../middlewares/asyncWrapper";
+import applicationsServices from "../services/applicationsServices";
+import paginationParams from "../utils/pagination/paginationParams";
+import httpStatusText from "../utils/httpStatusText";
+import { TApplication } from "../schemas/applicationSchema";
+
+const getAllApplications = asyncWrapper(
+  async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void | Response> => {
+    const query = req.query;
+
+    const pagination = paginationParams(query);
+
+    const { paginationData, applications } =
+      await applicationsServices.getApplicationsService(pagination);
+
+    return res.status(200).json({
+      status: httpStatusText.SUCCESS,
+      data: { applications, paginationData },
+    });
+  }
+);
+
+const createApplication = asyncWrapper(
+  async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void | Response> => {
+    const applicationData = req.validatedData as TApplication;
+
+    const createdApplication =
+      await applicationsServices.createApplicationService(applicationData);
+
+    return res.status(201).json({
+      status: httpStatusText.SUCCESS,
+      data: { createdApplication },
+    });
+  }
+);
+
+export { getAllApplications, createApplication };
